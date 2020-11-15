904cf12cda03569dedf6ddc5fcc65caf0718d002
Alejandro Hijano <alexdlol619@gmail.com>
2020-11-15 12:39:29 +0100

    - AHORA COMPILA COÑO

--- Source/VRGestosMagia/VRGestosMagia.Build.cs
+++ Source/VRGestosMagia/VRGestosMagia.Build.cs
@@ -11,11 +11,9 @@ public class VRGestosMagia : ModuleRules
 		
 
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "Landscape", "LandscapeEditor" });
 		                                                                                                                                              
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AdvancedSessions", "VRExpansionPlugin" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
