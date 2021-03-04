ee620563831fa3a0290335b90f46214a67466d36
Alejandro Hijano <alexdlol619@gmail.com>
2021-03-04 20:26:25 +0100

    - Mejora de variantes de colores en los mats del landscape, y de los arboles

--- Source/VRGestosMagiaEditor.Target.cs
+++ Source/VRGestosMagiaEditor.Target.cs
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+ld // Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
 using System.Collections.Generic;
@@ -12,22 +12,5 @@ public class VRGestosMagiaEditorTarget : TargetRules
         Type = TargetType.Editor;
         ExtraModuleNames.AddRange(new string[] { "VRGestosMagia" });
 		
-		//bCompileChaos = true;
-		//bUseChaos = true;
-		//bUseUnityBuild = false;
-		//bUsePCHFiles = false;
     }
 
-	//
-	// TargetRules interface.
-	//
-
-/*	public override void SetupBinaries(
-		TargetInfo Target,
-		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
-		ref List<string> OutExtraModuleNames
-		)
-	{
-		OutExtraModuleNames.AddRange( new string[] { "VRGestosMagia" } );
-	}*/
-}
