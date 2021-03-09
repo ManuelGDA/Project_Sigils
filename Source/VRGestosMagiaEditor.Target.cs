bd48cb0060e07fdb72fccb054d5580e3d8f8f941
Alejandro Hijano <alexdlol619@gmail.com>
2021-03-09 14:41:41 +0100

    - Compilado, ni se ve el cel shader en compilado, ni las sombras se renderizan en el ojo derecho, ni la hierba se renderiza a la distancia correcta

--- Source/VRGestosMagiaEditor.Target.cs
+++ Source/VRGestosMagiaEditor.Target.cs
@@ -1,4 +1,4 @@
-ld // Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project Settings.
 
 using UnrealBuildTool;
 using System.Collections.Generic;
@@ -13,4 +13,4 @@ public class VRGestosMagiaEditorTarget : TargetRules
         ExtraModuleNames.AddRange(new string[] { "VRGestosMagia" });
 		
     }
-
+}
